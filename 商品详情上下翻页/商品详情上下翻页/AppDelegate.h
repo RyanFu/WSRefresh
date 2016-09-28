574df571c637682ba4d65d5bcac2a2ee879d3ff0
784846767@qq.com <784846767@qq.com>
2016-09-28 14:14:24 +0800

    .

---
+++ 商品详情上下翻页/商品详情上下翻页/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  商品详情上下翻页
+//
+//  Created by iMac on 16/9/28.
+//  Copyright © 2016年 zws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 商品详情上下翻页/商品详情上下翻页/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  商品详情上下翻页
+//
+//  Created by iMac on 16/9/28.
+//  Copyright © 2016年 zws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
